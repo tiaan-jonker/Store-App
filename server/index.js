e30da6a44232217cca029465ea37b7a81bd2800b
tiaan-jonker <tiaanj06@gmail.com>
2022-07-15 10:18:15 +1200

    Connecting mongodb to server

--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const colors = require('colors')
 const { graphqlHTTP } = require('express-graphql')
+require('dotenv').config()
 
+const connectDB = require('./config/dbConfig')
 const schema = require('./schema/schema')
 
 const PORT = 5000
 
 const server = express()
+
+connectDB()
 
 server.use(bodyParser.json())
 
